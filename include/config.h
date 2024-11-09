ae06766c4bcb49fd04156ea1f0bcde387d00de33
braedonwatkins <watkins.braedon@gmail.com>
2024-11-08 19:27:25 -0500

    modularization: configs

---
+++ include/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//WIFI creds
+extern const char* WIFI_SSID;
+extern const char* WIFI_PASS;
+
+// OWM API Config
+extern const char* OWM_API_KEY;
+extern const char* TEMP_UNIT;
+extern const char* CITY;
+
+//Timing Config
+extern const unsigned long SENSOR_DELAY_MS;
+extern const unsigned long OWM_DELAY_MS;
+
+//BME I2C Address
+extern const uint8_t BME280_ADDRESS;
